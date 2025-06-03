1e2824bb7872a636fec84c53d71ea51f6ebd6176
christopher sanders <ch.sa911@gmail.com>
2025-06-03 20:52:59 +0200

    Improve LOGIC

--- backend/logic/CreateBMark.js
+++ backend/logic/CreateBMark.js
@@ -1,11 +1,12 @@
 
-
+//Create a Bookmark
         const createBMark
-
+//Variables that determine a Bookmark
  position = String;
  colour = String;
  barcode = string;
 
+//Get Values from Inputfield when creating a book
         getBHoehe(e.target.value)   if BHoehe = 1
         then position = e,
         s
@@ -13,6 +14,9 @@
 
         then colour = b,
 
+        //Get barcode: Start with 1 rank 001 and check in database if value is unique
+
+//Konkatenate Values to complete Bookmark
         position+colour+barcode;
-
-
+// Pass Booksmark to Inputfield
+        setBMark;
