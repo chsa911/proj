864626a065dc744e97d9ba5ff32bade3a9a19847
chsa911 <ch.sa911@gmail.com>
2025-06-05 16:16:19 +0200

    improve logic

--- backend/logic/CreateBMark.js
+++ backend/logic/CreateBMark.js
@@ -1,18 +1,28 @@
+import React, { useEffect, useState } from "react";
+
 
 //Create a Bookmark
-        const createBMark
+        const createBMark() {
 //Variables that determine a Bookmark
  position = String;
  colour = String;
  barcode = string;
+}
+//Get Values for from Inputfield when creating a book
+        getBHoehe(e.target.value)
+        getBBreite(e.target.value)
 
-//Get Values from Inputfield when creating a book
-        getBHoehe(e.target.value)   if BHoehe = 1
+//Check postion of BMar
+         if( BHoehe = 1){
         then position = e,
-        s
-        getBBreite(e.target.value)  if BBreite = 1
+        }else[
 
+        ]
+
+        if( BBreite = 1){
         then colour = b,
+        }
+
 
         //Get barcode: Start with 1 rank 001 and check in database if value is unique
 
