a2ad552bcb4b7b3b42032d71646410ec08162fd3
chsa911 <ch.sa911@gmail.com>
2025-06-05 17:58:24 +0200

    improve logic

--- backend/logic/CreateBMark.js
+++ backend/logic/CreateBMark.js
@@ -4,29 +4,36 @@ import React, { useEffect, useState } from "react";
 //Create a Bookmark
         const createBMark() {
 //Variables that determine a Bookmark
+//position depends on the BHohe and BBreite
+
  position = String;
  colour = String;
  barcode = string;
 }
-//Get Values for from Inputfield when creating a book
+//Get Values from Inputfield when creating a book
         getBHoehe(e.target.value)
         getBBreite(e.target.value)
 
-//Check postion of BMar
-         if( BHoehe = 1){
+//GEt postion and colour of BMark
+         if( BBreite <= 10,5 and BHoehe <= 17,5 ){
         then position = e,
-        }else[
-
-        ]
-
-        if( BBreite = 1){
-        then colour = b,
+        and colour = b;
+        }else if { BBreite <= 10,5 and BHoehe >17,5 {
+        then position ist o, and colour =b;
         }
 
-
-        //Get barcode: Start with 1 rank 001 and check in database if value is unique
-
+        }
 //Konkatenate Values to complete Bookmark
-        position+colour+barcode;
+let barcode = 001;
+
+let BMArk = position+colour+barcode;
+
+//Uniquenesstest of BMark in DB If BMark is already there then change to next ranked barcode:
+
+
+
+
+
+
 // Pass Booksmark to Inputfield in CreateBooks
         setBMark;
