e031c5d492ad4084b89081bc559030f41251d28d
chsa911 <ch.sa911@gmail.com>
2025-06-08 22:01:16 +0200

    implement autocomplete files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,65 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import CreateBook from './pages/CreateBooks';
-import ShowBook from './pages/ShowBook';
-import EditBook from './pages/EditBook';
-import DeleteBook from './pages/DeleteBook';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CreateBook from "./pages/CreateBooks";
+import ShowBook from "./pages/ShowBook";
+import EditBook from "./pages/EditBook";
+import DeleteBook from "./pages/DeleteBook";
+import Autocomplete from "./components/autocomplete";
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/books/create' element={<CreateBook />} />
-      <Route path='/books/details/:id' element={<ShowBook />} />
-      <Route path='/books/edit/:id' element={<EditBook />} />
-      <Route path='/books/delete/:id' element={<DeleteBook />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
     </Routes>
+  );
+  const staticData = [
+    "apple",
+    //   "banana",
+    //   "berrl",
+    //   "orange",
+    //   "grape",
+    //   "mango",
+    //   "melon",
+    //   "berry",
+    //   "peach",
+    //   "cherry",
+    //   "plum",
+  ];
+
+  const fetchSuggestions = async (query) => {
+    const response = await fetch(
+      `https://dummyjson.com/recipes/search?q=${query}`
+    );
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const result = await response.json();
+    return result.recipes;
+  };
+
+  return (
+    <div>
+      <h1>Autocomplete / Typeahead</h1>
+
+      <Autocomplete
+        placeholder={"Enter Recipe"}
+        staticData={staticData}
+        fetchSuggestions={fetchSuggestions}
+        dataKey={"name"}
+        customLoading={<>Loading Recipes..</>}
+        onSelect={(res) => console.log(res)}
+        caching={true}
+        onChange={(input) => {}}
+        onBlur={(e) => {}}
+        onFocus={(e) => {}}
+        customStyles={{}}
+      />
+    </div>
   );
 };
 
